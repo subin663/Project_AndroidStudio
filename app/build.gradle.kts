3a23a431a2dc3a1572798e25b56c6e2542cc63a5
Vinh <lvinh5559@gmail.com>
2025-04-23 22:47:52 +0700

    ud

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.android.build.api.dsl.ApplicationBuildFeatures
+
 plugins {
     alias(libs.plugins.android.application)
 }
@@ -29,6 +31,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -37,6 +42,9 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(group = "com.github.ismaeldivita", name = "chip-navigation-bar", version = "1.4.0")
+    implementation(group = "androiddx.viewpaper2", name = "viewpaper2", version = "1.0.0")
+    implementation(group = "com.github.bumptech.glide", name = "glide", version = "4.12.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
