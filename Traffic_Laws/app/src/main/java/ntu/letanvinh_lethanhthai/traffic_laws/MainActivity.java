3e198c42f27c7fa45c29d628faf8c60fec6cec3d
Vinh <lvinh5559@gmail.com>
2025-05-07 01:03:48 +0700

    update

--- Traffic_Laws/app/src/main/java/ntu/letanvinh_lethanhthai/traffic_laws/MainActivity.java
+++ Traffic_Laws/app/src/main/java/ntu/letanvinh_lethanhthai/traffic_laws/MainActivity.java
@@ -1,24 +1,55 @@
 package ntu.letanvinh_lethanhthai.traffic_laws;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    BottomNavigationView bottomNav;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        bottomNav=findViewById(R.id.bottomNavigationView);
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.fragment_container, new OntapFragment()).commit();
+
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                Fragment selectedFragment=null;
+                int mnuItemDuocChonID=item.getItemId();
+                if (mnuItemDuocChonID==R.id.mnu_thithu){
+                    //thay fragment
+                    fragmentManager.beginTransaction().replace(R.id.bottomNavigationView,new ThithuFragment()).commit();
+                    Toast.makeText(MainActivity.this, "Thay Home",Toast.LENGTH_LONG).show();
+                }
+                else if(mnuItemDuocChonID==R.id.mnu_ontap){
+                    fragmentManager.beginTransaction().replace(R.id.bottomNavigationView,new OntapFragment()).commit();
+                    Toast.makeText(MainActivity.this,"Thay Search",Toast.LENGTH_LONG).show();
+                }
+                else if(mnuItemDuocChonID==R.id.mnu_bienbao){
+                    fragmentManager.beginTransaction().replace(R.id.bottomNavigationView,new BienbaoFragment()).commit();
+                    Toast.makeText(MainActivity.this,"Thay Profile",Toast.LENGTH_LONG).show();
+                }
+                else{
+                    return true;
+                }
+                return false;
+            }
         });
+
     }
 }
