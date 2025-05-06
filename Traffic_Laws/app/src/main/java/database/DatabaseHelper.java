f18eac2a99796a3d1c5a3c9e8dd0268ff9946b60
Vinh <lvinh5559@gmail.com>
2025-05-06 23:33:01 +0700

    ud

---
+++ Traffic_Laws/app/src/main/java/database/DatabaseHelper.java
@@ -0,0 +1,17 @@
+package database;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
