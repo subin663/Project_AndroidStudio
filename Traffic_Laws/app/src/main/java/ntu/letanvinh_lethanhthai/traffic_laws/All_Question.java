94f6a54f39d0a9befbfd8ab2a61dbca2a798468f
LeThanhThai-64132120 <thai.lt.64cntt@ntu.edu.vn>
2025-05-14 21:40:39 +0700

    Thực hiện thêm câu hỏi ÔN TẬP

--- Traffic_Laws/app/src/main/java/ntu/letanvinh_lethanhthai/traffic_laws/All_Question.java
+++ Traffic_Laws/app/src/main/java/ntu/letanvinh_lethanhthai/traffic_laws/All_Question.java
@@ -1,18 +1,18 @@
 package ntu.letanvinh_lethanhthai.traffic_laws;
 
-import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.List;
 
 public class All_Question {
     private String question;
     private List<String> options;
     private String answer;
+    private String image;
 
-    public All_Question(String question, List<String> options, String answer) {
+    public All_Question(String question, List<String> options, String answer, String image) {
         this.question = question;
         this.options = options;
         this.answer = answer;
+        this.image = image;
     }
 
     // Các phương thức getter lấy dữ liệu
@@ -28,15 +28,7 @@ public class All_Question {
         return answer;
     }
 
-    public void setQuestion(String question) {
-        this.question = question;
-    }
-
-    public void setOptions(List<String> options) {
-        this.options = options;
-    }
-
-    public void setAnswer(String answer) {
-        this.answer = answer;
+    public String getImage() {
+        return image;
     }
 }
